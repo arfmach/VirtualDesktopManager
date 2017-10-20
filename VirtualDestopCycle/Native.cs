aca2a5366e9deb4cb9ea0a028b9f3f2f22ebde6c
arfmach <alexandre.r.f.machado@gmail.com>
2017-10-20 13:37:54 -0200

    Code normalization and formatting

--- VirtualDestopCycle/Native.cs
+++ VirtualDestopCycle/Native.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 
 namespace VirtualDesktopManager
 {
     internal static class Native
     {
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
         internal static extern int SystemParametersInfo(
             uint uAction,
             uint uParam,
